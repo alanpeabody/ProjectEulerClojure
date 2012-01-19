06788a1602a424a2e4dba0e0127b5985c7e895bd
Alan Peabody <gapeabody@gmail.com>
2012-01-18 22:54:51 -0500

    Problem two solved.

--- src/project_euler/core.clj
+++ src/project_euler/core.clj
@@ -2,9 +2,15 @@
 
 (def whole-numbers (iterate inc 1))
 
+(def fib (map first (iterate (fn [[a b]] [b (+ a b)]) [0 1])))
+
 (defn one
   "Returns the sum of all multiple of 3 or 5 from 0 to range end"
   [end]
   (let [multiple? #(or (= 0 (mod % 3)) (= 0 (mod % 5)))]
     (reduce + 0 (filter multiple? (take end whole-numbers)))))
 
+(defn two
+  "Returns the sum of all even numbers of the fib seq under max"
+  [max]
+  (reduce + 0 (filter even? (take-while #(< % max) fib))))
--- test/project_euler/test/core.clj
+++ test/project_euler/test/core.clj
@@ -5,3 +5,8 @@
 (deftest test-problem-one
          (is (= (project-euler.core/one 9) 23))
          (is (= (project-euler.core/one 999) 233168)))
+
+(deftest test-problem-two
+         (is (= (project-euler.core/two 20) 10))
+         (is (= (project-euler.core/two 4000000) 4613732)))
+
